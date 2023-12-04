bf6da74117fbc0e33f913dd4330e8f541d8c4cee
Matt <mattcalhoun1@gmail.com>
2023-12-04 07:35:18 -0500

    fix nearest dist calc

--- lvps/gym/lvps_gym_rewards.py
+++ lvps/gym/lvps_gym_rewards.py
@@ -27,6 +27,7 @@ class LvpsGymRewards:
         self.__target_closer_counts = {}
         self.__target_photo_distance_counts = {}
         self.__target_photo_counts = {}
+        self.__target_last_dist = {}
 
     
     def calculate_reward (self, action_performed, action_result, target_found : bool, target_found_by_this_agent : bool, all_targets_found : bool, beg_nearest_unfound_target_id, beg_nearest_unfound_target_dist : float, end_nearest_unfound_target_id, end_nearest_unfound_target_dist : float, is_within_photo_distance : bool):
@@ -57,11 +58,13 @@ class LvpsGymRewards:
             if end_nearest_unfound_target_id is not None and end_nearest_unfound_target_id not in self.__target_find_counts:
                 reward += RewardAmounts.SuccessfulLook
                 self.__target_find_counts[end_nearest_unfound_target_id] = 1
+                self.__target_last_dist[end_nearest_unfound_target_id] = end_nearest_unfound_target_dist
                 logging.getLogger(__name__).info(f"Agent rewarded for first time sighting of {end_nearest_unfound_target_id}")
-            # have we closed the distance to the target in question
-            elif end_nearest_unfound_target_id is not None and end_nearest_unfound_target_id == beg_nearest_unfound_target_id and end_nearest_unfound_target_dist < beg_nearest_unfound_target_dist:
-                if end_nearest_unfound_target_id not in self.__target_closer_counts:
+            # have we closed the distance to the target in question, get rewarded for first time
+            elif end_nearest_unfound_target_id is not None and end_nearest_unfound_target_id not in self.__target_closer_counts:
+                if is_within_photo_distance == False and end_nearest_unfound_target_dist < self.__target_last_dist[end_nearest_unfound_target_id]:
                     self.__target_closer_counts[end_nearest_unfound_target_id] = 1
+                    self.__target_last_dist[end_nearest_unfound_target_dist] = end_nearest_unfound_target_dist # also tracking in case we want to reward more distance closing at some point
                     logging.getLogger(__name__).info(f"Agent rewarded for closing distance to {end_nearest_unfound_target_id}")
                     reward += RewardAmounts.SuccessfulCloserLook
             
