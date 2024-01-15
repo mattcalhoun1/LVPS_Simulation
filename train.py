162f294bf54ebedafb2e7f911adb519f99b35439
Matt <mattcalhoun1@gmail.com>
2024-01-15 07:19:32 -0500

    bump training steps

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ class Train:
         self.__model_dir = model_dir
         self.__max_episode_steps = 1000 # max steps per episode
         self.__max_test_steps = 1000 # max steps per episode
-        self.__max_total_steps = 3_000_000
+        self.__max_total_steps = 10_000_000
         self.__test_episodes = 3
 
         # create new instances of the environment
